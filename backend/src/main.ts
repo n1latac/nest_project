eedea8b7d959145bb3f22e7d2564be1cde251563
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2024-04-25 13:23:31 +0300

    changes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000, () => {
-    console.log('server start on port 3000');
+    console.log('server start on port 4000');
   });
 }
 bootstrap();
