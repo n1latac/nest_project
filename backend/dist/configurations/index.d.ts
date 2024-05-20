2b4968014a0a035e7cc4b9a98f7edae4709b8174
vladpoltavskiy <vpoltavskiy0y@gmail.com>
2024-05-20 03:28:45 +0300

    dist

--- backend/dist/configurations/index.d.ts
+++ backend/dist/configurations/index.d.ts
@@ -1,4 +1,9 @@
 declare const _default: () => {
     port: string;
+    db_host: string;
+    db_port: string;
+    db_name: string;
+    db_user: string;
+    db_password: string;
 };
 export default _default;
--- backend/dist/configurations/index.js
+++ backend/dist/configurations/index.js
@@ -2,5 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = () => ({
     port: process.env.PORT,
+    db_host: process.env.DB_HOST,
+    db_port: process.env.DB_PORT,
+    db_name: process.env.DB_NAME,
+    db_user: process.env.DB_USER,
+    db_password: process.env.DB_PASSWORD,
 });
 //# sourceMappingURL=index.js.map
