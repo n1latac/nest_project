36e2e9d8a63daa1864a21a7bcc3b7be7d6a41ea1
Vlad Poltavskiy <vpoltavskiy0y@gmail.com>
2024-06-12 13:38:16 +0400

    styles

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -5,6 +5,7 @@ import {NavLink, useLocation, useNavigate} from "react-router-dom";
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
 import {login, registration} from "../api/userApi";
+import './assets/style.css'
 
 const Auth = observer(() => {
     const {user} = useContext(Context)
@@ -31,44 +32,46 @@ const Auth = observer(() => {
 
     }
     return (
-        <Container className="d-flex justify-content-center align-items-center vh-100">
-            <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">{isLogin ? 'Авторизация' : "Регистрация"}</h2>
-                <Form className="d-flex flex-column">
-                    <Form.Control
-                        className="mt-3"
-                        placeholder="Введите ваш email..."
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <Form.Control
-                        className="mt-3"
-                        placeholder="Введите ваш пароль..."
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        type="password"
-                    />
-                    <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
-                        {isLogin ?
-                            <div>
-                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
-                            </div>
-                            :
-                            <div>
-                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
-                            </div>
-                        }
-                        <Button
-                            variant={"outline-success"}
-                            onClick={click}
-                        >
-                            {isLogin ? 'Войти' : 'Регистрация'}
-                        </Button>
-                    </Row>
-                </Form>
-            </Card>
-        </Container>
-
+        <Container className="d-flex justify-content-center align-items-center vh-100 custom-overlay">
+        <Card style={{width: 600}} className="p-5 custom-card">
+            <h2 className="m-auto">{isLogin ? 'Authentication' : "Registration"}</h2>
+            <Form className="d-flex flex-column">
+                <Form.Control
+                    className="mt-3 custom-form-control"
+                    placeholder="Enter your email..."
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <Form.Control
+                    className="mt-3 custom-form-control"
+                    placeholder="Enter your password..."
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    type="password"
+                />
+                <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+                    {isLogin ?
+                        <div>
+                            Don't have an account? <NavLink className="custom-link" to={REGISTRATION_ROUTE}>Sign Up!</NavLink>
+                        </div>
+                        :
+                        <div>
+                            Already have an account? <NavLink className="custom-link" to={LOGIN_ROUTE}>Log In!</NavLink>
+                        </div>
+                    }
+                    <Button
+                        variant={"outline-success"}
+                        onClick={click}
+                        className="custom-button"
+                    >
+                        {isLogin ? 'Log In' : 'Sign Up'}
+                    </Button>
+                </Row>
+            </Form>
+        </Card>
+    </Container>
+    
+    
     );
 });
 
